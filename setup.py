511edb9d51ec7a748299986f2f3c5065c03aaa71
rishit <rishitbaliyan1015@gmail.com>
2024-12-18 15:09:09 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical_Chatbot',
+    version= '0.0.0',
+    author= 'Rishit Baliyan',
+    author_email= 'rishitbaliyan1015@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
